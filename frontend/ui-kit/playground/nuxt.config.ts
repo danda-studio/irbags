d82b23d27949acf67b15650a6c34d5f99c5b1a39
Denis <90174245+KossaDenis@users.noreply.github.com>
2026-01-08 21:15:32 +0200

    [Nb05iFA0] - UI Kit / Typography (#15)
    * adding a typography
    
    * adding a typography

--- frontend/ui-kit/playground/nuxt.config.ts
+++ frontend/ui-kit/playground/nuxt.config.ts
@@ -1,5 +1,5 @@
 export default defineNuxtConfig({
   modules: ["../src/module.ts"],
   devtools: { enabled: true },
-  css: ['../src/styles/index.css']
+  css: ["../src/styles/index.css"],
 });
--- frontend/ui-kit/src/runtime/components/FileInput/FileInput.vue
+++ frontend/ui-kit/src/runtime/components/FileInput/FileInput.vue
@@ -1,9 +1,9 @@
 <template>
   <IBGFileUpload :file-delete="false" accept="image/*" icon="ibg:image">
-    <template #files-top="{ removeFile }">
+    <template #files-top="{ open }">
       <p
         class="text-black z-10 font-medium absolute top-5 left-5"
-        @click="removeFile()"
+        @click="open()"
       >
         заменить
       </p>
