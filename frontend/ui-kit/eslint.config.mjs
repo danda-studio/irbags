4eec93cee0d0d4b680484a073e9d1d7a107000dc
Nikita Nedelko <93500964+weekkkk@users.noreply.github.com>
2026-01-11 14:23:03 +0200

    YxySnxyF: Fix UI Kit Eslint (#17)

--- frontend/ui-kit/eslint.config.mjs
+++ frontend/ui-kit/eslint.config.mjs
@@ -1,33 +1,46 @@
 // @ts-check
 import antfu from "@antfu/eslint-config";
+import withNuxt from "./.nuxt/eslint.config.mjs";
 
-export default antfu({
-  stylistic: {
-    indent: 2, // 4, or 'tab'
-    quotes: "double", // or 'double'
-    semi: true,
-  },
-  vue: {
-    overrides: {
-      "vue/no-multiple-template-root": "off",
+export default withNuxt(
+  antfu(
+    {
+      stylistic: {
+        indent: 2, // 4, or 'tab'
+        quotes: "double", // or 'double'
+        semi: true,
+      },
+      vue: {
+        overrides: {
+          "vue/no-multiple-template-root": "off",
+        },
+      },
+      formatters: {
+        /**
+         * Format CSS, LESS, SCSS files, also the `<style>` blocks in Vue
+         * By default uses Prettier
+         */
+        css: true,
+        /**
+         * Format HTML files
+         * By default uses Prettier
+         */
+        html: true,
+        /**
+         * Format Markdown files
+         * Supports Prettier and dprint
+         * By default uses Prettier
+         */
+        markdown: "prettier",
+      },
+    },
+  ),
+  {
+    files: ["nuxt.config.ts"],
+    languageOptions: {
+      globals: {
+        defineNuxtConfig: "readonly",
+      },
     },
   },
-  formatters: {
-    /**
-     * Format CSS, LESS, SCSS files, also the `<style>` blocks in Vue
-     * By default uses Prettier
-     */
-    css: true,
-    /**
-     * Format HTML files
-     * By default uses Prettier
-     */
-    html: true,
-    /**
-     * Format Markdown files
-     * Supports Prettier and dprint
-     * By default uses Prettier
-     */
-    markdown: "prettier",
-  },
-});
+);
--- frontend/ui-kit/nuxt.config.ts
+++ frontend/ui-kit/nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/ui"],
+  modules: ["@nuxt/ui", "@nuxt/eslint"],
   ui: {
     prefix: "IBG",
+  },
+  eslint: {
+    config: {
+      standalone: false,
+    },
   },
 });
