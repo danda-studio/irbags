3bbe86cf5554b6369a6200a02e22c31f9737323e
Denis Kossa <denis.kosssa@gmail.com>
2026-01-12 21:16:11 +0200

    Test work

--- frontend/admin/src/app/routes/index.vue
+++ frontend/admin/src/app/routes/index.vue
@@ -1,3 +1,21 @@
+<script lang="ts" setup>
+const bannerKeys = {
+  main: "bannerMain",
+  banner1: "banner1",
+  banner2: "banner2",
+  banner3: "banner3",
+  footer: "bannerFooter",
+};
+</script>
+
 <template>
-  <div />
+  <div class="h-screen">
+    <BannerFeature :banner-key="bannerKeys.main" class="w-full h-full" />
+    <div class="flex h-screen">
+      <BannerFeature :banner-key="bannerKeys.banner1" class="w-full h-full" />
+      <BannerFeature :banner-key="bannerKeys.banner2" class="w-full h-full" />
+    </div>
+    <BannerFeature :banner-key="bannerKeys.banner3" class="w-full h-225" />
+    <BannerFeature :banner-key="bannerKeys.footer" class="w-full h-screen" />
+  </div>
 </template>
---
+++ frontend/admin/src/features/banner/ui/BannerFeature.public.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+import type { BannerFeatureProps } from "./interfaces";
+
+/** Параметры */
+const props = defineProps<BannerFeatureProps>();
+</script>
+
+<template>
+  <IBGFileInput class="h-full" />
+</template>
