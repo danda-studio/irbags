4f779f16f3e3fa7a0af90c3ab6d5774bf0fc2c88
Denis Kossa <denis.kosssa@gmail.com>
2026-01-12 23:11:57 +0200

    create banners

--- frontend/admin/src/widgets/layout/header/ui/LayoutHeaderWidget.public.vue
+++ frontend/admin/src/widgets/layout/header/ui/LayoutHeaderWidget.public.vue
@@ -6,13 +6,14 @@ const navigationItems = Object.keys(routesMap)
 </script>
 
 <template>
-  <header class="flex items-center justify-between pt-3 p-5 pb-0 gap-52.5">
+  <header
+    class="fixed top-0 left-0 right-0 z-10 flex items-center justify-between mt-3 m-5 mb-0! gap-52.5 max-lg:m-6 max-lg:mt-5 max-lg:gap-50">
     <div class="flex items-center justify-between w-full">
       <IBGInput class="w-full" placeholder="поиск" />
-      <IBGRadioGroup :ui="{ container: 'hidden', wrapper: 'ml-0', fieldset: 'gap-5', label: 'text-base' }"
+      <IBGRadioGroup :ui="{ container: 'hidden', wrapper: 'ml-0', fieldset: 'gap-5 max-lg:gap-6', label: 'text-base' }"
         :items="navigationItems" orientation="horizontal" v-model="selected" />
     </div>
-    <div class="flex items-center justify-between w-full gap-159.5">
+    <div class="flex items-center justify-between w-full gap-159.5 max-lg:gap-158">
       <div class="flex items-center gap-17.75">
         <IBGButton>добавить</IBGButton>
         <IBGButton>удалить</IBGButton>
