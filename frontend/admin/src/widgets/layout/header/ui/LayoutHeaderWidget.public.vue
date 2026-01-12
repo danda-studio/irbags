38a91cf646ff589cbcef16b02fc1a52ac966387e
vyusal <vyusalb@inbox.ru>
2026-01-12 02:20:59 +0200

    feature/0EpUC0hG create route products and create widgets ProductsPageWidget.public.vue

--- frontend/admin/src/widgets/layout/header/ui/LayoutHeaderWidget.public.vue
+++ frontend/admin/src/widgets/layout/header/ui/LayoutHeaderWidget.public.vue
@@ -1,5 +1,15 @@
 <script lang="ts" setup>
 const items = ["главная", "товары", "фильтры"];
+
+function onSelect(item: string) {
+  if (item === "товары") {
+    router.push('/products')
+  } else if (item === "главная") {
+    router.push('/') // если есть главная страница
+  } else if (item === "фильтры") {
+    router.push('/filters') // пример, если есть
+  }
+}
 </script>
 
 <template>
@@ -8,7 +18,8 @@ const items = ["главная", "товары", "фильтры"];
       <IBGInput class="w-full" placeholder="поиск" />
       <IBGRadioGroup
         :ui="{ container: 'hidden', wrapper: 'ml-0', fieldset: 'gap-5', label: 'text-base' }"
-        :items="items" orientation="horizontal"
+        :items="items"
+        orientation="horizontal"
       />
     </div>
     <div class="flex items-center justify-between w-full gap-159.5">
---
+++ frontend/admin/src/widgets/products/ui/ProductsPageWidget.public.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>GOOOD</h1>
+</template>
