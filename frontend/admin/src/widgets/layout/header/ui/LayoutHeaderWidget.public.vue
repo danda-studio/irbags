9a45fb733a4be89df6fcaba36c8bf0b93f406bbf
Denis Kossa <denis.kosssa@gmail.com>
2026-01-16 23:01:11 +0200

    fix

--- frontend/admin/src/widgets/layout/header/ui/LayoutHeaderWidget.public.vue
+++ frontend/admin/src/widgets/layout/header/ui/LayoutHeaderWidget.public.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
-import { useNavigation } from '~/shared/composables/useNavigation'
-
-const { selected, navigationItems } = useNavigation();
+  const { selected } = useNavigation();
+/**Элементы навигации */
+const navigationItems = Object.keys(routesMap)
 
 </script>
 
@@ -9,12 +9,8 @@ const { selected, navigationItems } = useNavigation();
   <header class="flex items-center justify-between pt-3 p-5 pb-0 gap-52.5">
     <div class="flex items-center justify-between w-full">
       <IBGInput class="w-full" placeholder="поиск" />
-      <IBGRadioGroup
-        :ui="{ container: 'hidden', wrapper: 'ml-0', fieldset: 'gap-5', label: 'text-base' }"
-        :items="navigationItems"
-        orientation="horizontal"
-        v-model="selected"
-      />
+      <IBGRadioGroup :ui="{ container: 'hidden', wrapper: 'ml-0', fieldset: 'gap-5', label: 'text-base' }"
+        :items="navigationItems" orientation="horizontal" v-model="selected" />
     </div>
     <div class="flex items-center justify-between w-full gap-159.5">
       <div class="flex items-center gap-17.75">
