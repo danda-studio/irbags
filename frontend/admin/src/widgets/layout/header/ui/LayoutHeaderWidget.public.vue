cb25ea61ee4a6fa7545af023caea7917d7a59b77
Denis <90174245+KossaDenis@users.noreply.github.com>
2026-01-19 22:22:44 +0200

    [NHatbbqU] - Admin / Banners (#20)
    * Test work
    
    * create banners
    
    * fix header size
    
    * fix
    
    * Test work
    
    * fix

--- frontend/admin/src/widgets/layout/header/ui/LayoutHeaderWidget.public.vue
+++ frontend/admin/src/widgets/layout/header/ui/LayoutHeaderWidget.public.vue
@@ -1,19 +1,21 @@
 <script lang="ts" setup>
-  const { selected } = useNavigation();
+const { selected } = useNavigation();
 /**Элементы навигации */
 const navigationItems = Object.keys(routesMap)
 
 </script>
 
 <template>
-  <header class="flex items-center justify-between pt-3 p-5 pb-0 gap-52.5">
+  <header
+    class="fixed top-0 left-0 right-0 z-10 flex items-center justify-between mt-3 m-5 mb-0! gap-54 max-lg:m-5 max-lg:mt-4 max-lg:gap-38.5">
     <div class="flex items-center justify-between w-full">
       <IBGInput class="w-full" placeholder="поиск" />
-      <IBGRadioGroup :ui="{ container: 'hidden', wrapper: 'ml-0', fieldset: 'gap-5', label: 'text-base' }"
+      <IBGRadioGroup
+        :ui="{ container: 'hidden', wrapper: 'ml-0', fieldset: 'gap-5.5', label: 'text-base max-lg:text-xs' }"
         :items="navigationItems" orientation="horizontal" v-model="selected" />
     </div>
-    <div class="flex items-center justify-between w-full gap-159.5">
-      <div class="flex items-center gap-17.75">
+    <div class="flex items-center justify-between w-full gap-161 max-lg:gap-120">
+      <div class="flex items-center gap-17.75 max-lg:gap-14">
         <IBGButton>добавить</IBGButton>
         <IBGButton>удалить</IBGButton>
       </div>
