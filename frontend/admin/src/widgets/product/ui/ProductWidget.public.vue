b6968e72271d56d0f3319af9b34b09bd9d787202
Denis Kossa <denis.kosssa@gmail.com>
2026-01-19 22:45:44 +0200

    work

--- frontend/admin/src/widgets/product/ui/ProductWidget.public.vue
+++ frontend/admin/src/widgets/product/ui/ProductWidget.public.vue
@@ -7,15 +7,17 @@ type Product = {
 
 const products: Product[] = Array.from({ length: 16 }, (_, i) => ({
   id: i + 1,
-  title: `Сумка Kelly ${i + 1}`,
+  title: `сумка kelly ${i + 1}`,
   price: 69,
 }))
 </script>
 
 <template>
-  <section class="mt-55 w-screen px-0 pb-2">
-    <div class="grid grid-cols-4 gap-x-0 gap-y-5">
-      <IBGCard v-for="product in products" :key="product.id">
+  <section class="mt-65 max-lg:mt-45 w-screen px-0 pb-2">
+    <div class="grid grid-cols-4 gap-x-0 gap-y-11">
+      <IBGCard v-for="product in products" :key="product.id" :ui="{
+        footer: 'pt-4.5!'
+      }">
         <template #default>
           <div class="bg-secondary-500 w-90 h-138.75 lg:w-120 lg:h-170.75" />
         </template>
