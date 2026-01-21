e90dea77ba5e5409c3d462cc753a85d1626a18e3
Денис Косса <denis.kosssa@gmail.com>
2026-01-21 22:38:40 +0300

    work

--- frontend/admin/src/widgets/layout/header/ui/LayoutHeaderWidget.public.vue
+++ frontend/admin/src/widgets/layout/header/ui/LayoutHeaderWidget.public.vue
@@ -7,14 +7,15 @@ const navigationItems = Object.keys(routesMap)
 
 <template>
   <header
-    class="fixed top-0 left-0 right-0 z-10 flex items-center justify-between mt-3 m-5 mb-0! gap-52.5 max-lg:m-6 max-lg:mt-5 max-lg:gap-50">
+    class="fixed top-0 left-0 right-0 z-10 flex items-center justify-between mt-4 m-5 mb-0! gap-53.25 max-lg:m-5 max-lg:mt-4.5 max-lg:gap-38.5">
     <div class="flex items-center justify-between w-full">
       <IBGInput class="w-full" placeholder="поиск" />
-      <IBGRadioGroup :ui="{ container: 'hidden', wrapper: 'ml-0', fieldset: 'gap-5', label: 'text-base' }"
+      <IBGRadioGroup
+        :ui="{ container: 'hidden', wrapper: 'ml-0', fieldset: 'gap-5', label: 'text-base max-lg:text-xs' }"
         :items="navigationItems" orientation="horizontal" v-model="selected" />
     </div>
-    <div class="flex items-center justify-between w-full gap-159.5 max-lg:gap-158">
-      <div class="flex items-center gap-17.75">
+    <div class="flex items-center justify-between w-full gap-161 max-lg:gap-120">
+      <div class="flex items-center gap-17.75 max-lg:gap-13.75">
         <IBGButton>добавить</IBGButton>
         <IBGButton>удалить</IBGButton>
       </div>
