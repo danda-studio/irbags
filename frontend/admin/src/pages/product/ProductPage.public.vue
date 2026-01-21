da6d7ae7bdb4c1ff0959b3673d97e842977be199
Денис Косса <denis.kosssa@gmail.com>
2026-01-21 23:48:38 +0300

    work

---
+++ frontend/admin/src/pages/product/ProductPage.public.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div>
+        <div class="flex">
+            <div class="w-123.75 pl-5 pr-5">
+                <div>
+                    <div class="mt-55">описание продукта, материалы, ФУРНИТУРА И ПРОЧЕЕ</div>
+                    <div class="mt-7.5">размер</div>
+                </div>
+            </div>
+            <div>
+                <IBGFileInput class="w-232.5 h-screen max-lg:w-172.5 max-lg:h-225" />
+            </div>
+            <div class="w-123.75 pl-5 pr-5">
+            </div>
+        </div>
+        <div class="flex">
+            <div class="w-123.75 pl-5 pr-5" />
+            <div>
+                <IBGFileInput class="w-232.5 h-screen max-lg:w-172.5 max-lg:h-225" />
+            </div>
+            <div class="w-123.75 pl-5 pr-5" />
+        </div>
+        <div class="flex">
+            <IBGFileInput class="w-full h-screen max-lg:w-180 max-lg:h-225" />
+            <IBGFileInput class="w-full h-screen max-lg:w-180 max-lg:h-225" />
+        </div>
+        <div class="flex">
+            <IBGFileInput class="w-full h-screen max-lg:w-180 max-lg:h-225" />
+            <IBGFileInput class="w-full h-screen max-lg:w-180 max-lg:h-225" />
+        </div>
+    </div>
+</template>
