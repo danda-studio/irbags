6687a536cd57247b7b24b3aef5c812468abe472b
denis kossa <denis.kosssa@gmail.com>
2026-02-05 21:15:56 +0300

    work

--- frontend/admin/src/pages/product/ProductPage.public.vue
+++ frontend/admin/src/pages/product/ProductPage.public.vue
@@ -10,17 +10,51 @@ const data = ref<productData>({
     shortDescription: '',
     size: ''
 });
+const items = ref(["System", "Light", "Dark"]);
+const value = ref("0");
+const colors = ref<Record<string, string>>({});
+
+const colorIndexes = computed(() => {
+    if (!Object.keys(colors.value).length)
+        return [{ value: "0" }];
+    const items = Object.keys(colors.value).map(item => ({ value: item }));
+    if (items.at(-1)?.value) {
+        items.push({ value: String(items.length) });
+    }
+    return items;
+});
+
+function filterColors() {
+    if (Object.keys(colors.value).length === 1)
+        return;
+    colors.value = Object.entries(colors.value).reduce((acc, [key, value]) => {
+        if (value)
+            acc[key] = value;
+        return acc;
+    }, {} as Record<string, string>);
+}
 </script>
 
 <template>
     <div>
         <div class="flex">
-            <div class="w-123.75 pl-5 pr-5">
-                <div class="mt-55">
-                    <IBGTextarea v-model="data.detailsDescription"
-                        placeholder="описание продукта, материалы, фурнитура и прочее" autoresize class="w-full" />
+            <div class="flex flex-col justify-between w-123.75 pl-5 pr-5">
+                <div>
+                    <div class="mt-55">
+                        <IBGTextarea v-model="data.detailsDescription"
+                            placeholder="описание продукта, материалы, фурнитура и прочее" autoresize class="w-full" />
+                    </div>
+                    <IBGResizebleInput v-model="data.size" placeholder="размер" class="mt-7.5" />
                 </div>
-                <IBGResizebleInput v-model="data.size" placeholder="размер" class="mt-7.5" />
+                <div>
+                    <div>сумки</div>
+                    <div>ремни</div>
+                    <div>платки</div>
+                    <div>подвесы</div>
+                    <div>брелки</div>
+                    <div>обложки</div>
+                    <div>визитницы</div>
+                </div>
             </div>
             <div>
                 <IBGFileInput class="w-232.5 h-screen max-lg:w-172.5 max-lg:h-225" />
@@ -32,6 +66,13 @@ const data = ref<productData>({
                         <IBGResizebleInput v-model="data.price" placeholder="сумма" />
                         <IBGResizebleInput v-model="data.discount" placeholder="скидка" />
                     </div>
+                    <IBGRadioGroup class="mt-12.5" v-model="value" :items="colorIndexes"
+                        :ui="{ wrapper: 'ml-2', fieldset: 'gap-5.5', label: 'text-base max-lg:text-xs' }">
+                        <template #label="{ item }">
+                            <IBGInput v-model="colors[item.value]" placeholder="добавить цвет"
+                                @focus="value = item.value" @blur="filterColors" />
+                        </template>
+                    </IBGRadioGroup>
                 </div>
             </div>
         </div>
